f6646ce4c62898c574cc8bcc995e42ca834e89a7
Robin Windows PC <robin.proeckl@mailbox.org>
2024-10-24 13:40:47 +0200

    KAN-49 DFG-Klasse, Tiefensuche und XOR

---
+++ src/app/classes/RueckgabeDFGs.ts
@@ -0,0 +1,7 @@
+import { DirectlyFollows } from './DirectlyFollows';
+
+export interface RueckgabeDFGs {
+    dfg1: DirectlyFollows;
+    dfg2: DirectlyFollows;
+    verlinkung: unknown; // Ersetze "unknown" durch den gewünschten Typ
+}
