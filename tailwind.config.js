febd726140ec9eaf0edbd3e441dd344043a9bb96
Carl Julian Sauter <sauter@siemens.com>
2024-10-26 12:55:56 +0200

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.{ts,html}',
+    ],
+    corePlugins: {
+        preflight: false
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
